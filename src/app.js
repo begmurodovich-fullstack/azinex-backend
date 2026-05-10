aa131dd5a56a433afb63d8ecca7d05af07e48b63
begmurodovich <begmurodovich@kali.begmurodovich>
2026-05-11 02:15:37 +0500

    auth

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ app.use(
       : undefined,
   ),
 );
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Backend working");
